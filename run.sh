4529e5855e6ff963b530059339c621f100e699dd
Samspy-X <sambitfavspidey@gmail.com>
2025-09-26 14:46:27 +0000

    autosave

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd gtnh/ || exit
+
+# Start server
+sh startserver-java9.sh &
+
+# Start tailscale
+sudo tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+
+# Run autosave loop
+watch -n 300 'git add . && git commit -m "autosave" || true && git push origin main'
